68971afa0223d0fa69e3dfc7715d16f97abc01f0
CUI, SHUHAI <toshuhai@hotmail.com>
2015-07-09 14:14:30 -0700

    initial setup

---
+++ soco_ubuntu/soco_website/soco_introduction.php~
@@ -0,0 +1,7 @@
+
+<?php include 'includes/soco_header.php' ?>
+
+
+<h2> A Baby, Born with Power and Muscle </h2>
+
+<?php include 'includes/soco_footer.php' ?>
---
+++ soco_ubuntu/soco_website/soco_products.php
@@ -0,0 +1,80 @@
+
+
+<?php include 'includes/soco_header.php' ?>
+
+
+
+<div>
+<div style="width:180px;float:left;">
+<li> Video Management Software </li>
+</div>
+
+<div style=" width:900px;float:left;">
+
+<img src="./images/soco_software.png"  alt="Soco Surveillance Software"  style="float:left;">
+
+<li type="circle"> Runs on Windows, Linux, MacOS
+ </li>
+<li type="circle"> Watch footage in real time</li>
+<br>
+<li type="circle"> Intelligent motion detection and action: 
+</li>
+<li type="circle"> Supports a broad range of codec formats 
+</li>
+<li type="circle"> Customizable 1-16 Channel Mega HD display  
+</li>
+
+<br>
+
+    <img src="./images/soco_comin.png" alt="For detail info, see"  style="float:left;"> <a class="anchor" href="soco_products.php">Soco Product</a>
+
+</div>
+</div>
+
+<div style="clear:both;"></div>
+<br>
+<hr width="60%" align="left">
+<br>
+
+<div style="width:1000px;float:left;">
+<div style="width:200px;float:left;">
+<li> Streaming Management System </li>
+</div>
+
+<div style="width:800px;float:left;">
+
+<img src="./images/soco_camera.jpg"  alt="Windows Displayer"  style="float:left;">
+
+<li type="circle"> Manage network loadbalance</li>
+<li type="circle"> Combine streams together to save bandwidth, reduce client burden </li>
+<li type="circle"> Transcode, encode to needed format</li>
+<br>
+<br>
+<img src="./images/soco_comin.png" alt="For detail info, see"  style="float:left;"> <a class="anchor" href="soco_market.php" > Soco Market</a>
+
+</div>
+</div>
+
+
+<div style="width:1000px;float:left;">
+<div style="width:200px;float:left;">
+<li> Streaming Server </li>
+</div>
+
+<div style="width:800px;float:left;">
+
+<img src="./images/soco_camera.jpg"  alt="Windows Displayer"  style="float:left;">
+Soco streaming server works with comming NVR box, or resides in curstomers' machine.
+<li type="circle"> Provides web browsing Inteface</li>
+<li type="circle"> Sutible for various kind of situations  </li>
+<li type="circle"> Gives real time p2p or broadcast streaming </li>
+<br>
+<br>
+<img src="./images/soco_comin.png" alt="For detail info, see"  style="float:left;"> <a class="anchor" href="soco_market.php" > Soco Market</a>
+
+</div>
+</div>
+
+
+<?php include 'includes/soco_footer.php' ?>
+
